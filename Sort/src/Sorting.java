f31309096f1964a10dd53873c8f9546862cb51a7
Demmyzzz <33146623+Demmyzzz@users.noreply.github.com>
2018-09-22 15:07:34 +0300

    Add files via upload

--- Sort/src/Sorting.java
+++ Sort/src/Sorting.java
@@ -3,55 +3,64 @@ import java.util.Arrays;
 
 public class Sorting {
     public static void main(String[] args) {
-        int[] arr = new int[6];
-        int[][] arr2 = new int[6][2];
-        for (int i=0;i<arr.length;i++){ arr[i]= (int) (Math.random()*10);System.out.println(arr[i]); }
-        System.out.println("");
-        sort(arr,arr2 );
-    }
-
-    private static void sort(int[] arr, int[][] arr2) {
-        int maxRepeat=0;
+        ArrayList<Integer> list2 = new ArrayList<>();
         ArrayList<Integer> list = new ArrayList<>();
 
-        for (int i=0;i<arr.length;i++){
-            int sum=0;
+//        int[] arr = new int[6];
+        int[][] arr2 = new int[6][2];
 
-            for (int b=0;b<arr.length;b++){ if (arr[i]==arr[b]) { sum++; } }
-
-            arr2[i][0]=arr[i];
-            arr2[i][1]=sum;
-
-            System.out.print(arr2[i][0]+" element | ");
-            System.out.print(arr2[i][1]+" repeat | ");
-            if (arr2[i][1]>maxRepeat){
-                maxRepeat=arr2[i][1];
-            }
-            System.out.print(maxRepeat+" max repeat");
-            System.out.println(Arrays.toString(arr2[i]));
+        for (int i = 0; i < 6; i++) {
+            list2.add((int) (Math.random() * 10));
         }
-            for (int i=0;i<arr.length;i++){
-                if (arr2[i][1]==maxRepeat){
-                    System.out.print(arr2[i][0]+" - element ");
-                    System.out.print(arr2[i][1]+" - repeat | ");
-                    list.add(arr2[i][0]);
-                    for (int b=0;b<arr.length;b++) {
-                        if (arr[b] == arr2[i][1]) {
-                            list.add(arr[b]);
-                        }
-                        for (int o = 0; o < arr.length; o++) {
-                            if (arr[b]==list.indexOf(o)){
-                                list.remove(list.get(list.indexOf(o)));
-                            }
-                        }
-                    }
+        System.out.println(list2);
+        System.out.println("");
+        //sort(arr,arr2 );
+        sorti(list, list2, arr2);
+    }
+
+    private static void sorti(ArrayList<Integer> list, ArrayList<Integer> list2, int[][] arr2) {
+        int maxRepeat = 0;
+        for (int i = 0; i < list2.size(); i++) {
+            int sum = 0;
+
+            for (int b = 0; b < list2.size(); b++) {
+                if (list2.get(i) == list2.get(b)) {
+                    sum++;
                 }
             }
-            for (int i=0;i<arr.length;i++){
+
+            arr2[i][0] = list2.get(i);
+            arr2[i][1] = sum;
+
+            System.out.print(arr2[i][0] + " element | ");
+            System.out.println(arr2[i][1] + " repeat | ");
+            System.out.println(Arrays.toString(arr2[i]));
+            if (arr2[i][1] > maxRepeat) {
+                maxRepeat = arr2[i][1];
+            }
+        }
+        for (int i=0;i<list2.size();i++){
+            if (arr2[i][1]==maxRepeat){
+                System.out.print(arr2[i][0]+" - element ");
+                System.out.print(arr2[i][1]+" - repeat | ");
+                int num = arr2[i][0];
+                findMore(num,list,list2);
+            }
+        }
+        for (int i=0;i<6;i++){
                 if (arr2[i][1]<maxRepeat){
                     list.add(arr2[i][0]);
                 }
             }
         System.out.println(list);
+    }
+    private static void findMore(int num, ArrayList<Integer> list, ArrayList<Integer> list2) {
+        for(int i=0;i<list2.size();i++){
+            if (list2.get(i)==num){
+                list.add(num);
+                list2.remove(i);
+            }
+
+        }
     }
 }
